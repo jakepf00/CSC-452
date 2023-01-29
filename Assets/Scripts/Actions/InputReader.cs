0ec776ba45fa8e5e400ed2aa45757c0f8b8440cc
Jacob Pfeifer <jakepf00@gmail.com>
2023-01-29 10:09:20 -0600

    Added character control scripts

---
+++ Assets/Scripts/Actions/InputReader.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputReader : MonoBehaviour, Controls.IPlayerActions {
+    public Vector2 MovementValue { get; private set; }
+    public event Action JumpEvent;
+    Controls _controls;
+
+    void Start() {
+        _controls = new Controls();
+        _controls.Player.SetCallbacks(this);
+        _controls.Player.Enable();
+    }
+    void OnDestroy() {
+        _controls.Player.Disable();
+    }
+    public void OnJump(InputAction.CallbackContext context) {
+        if (context.performed) {
+            JumpEvent?.Invoke();
+        }
+    }
+    public void OnLook(InputAction.CallbackContext context) {}
+    public void OnMove(InputAction.CallbackContext context) {
+        MovementValue = context.ReadValue<Vector2>();
+    }
+}
--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -1,15 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStateMachine : StateMachine {
-    void Start()
-    {
-        
-    }
+    [field: SerializeField] public InputReader InputReader { get; private set; }
+    [field: SerializeField] public CharacterController CharacterController { get; private set; }
+    [field: SerializeField] public Animator Animator { get; private set; }
+    [field: SerializeField] public float MovomentSpeed { get; private set; } = 10.0f;
+    public Transform MainCameraTransform { get; private set; }
 
-    void Update()
-    {
-        
+    void Start() {
+        MainCameraTransform = Camera.main.transform;
+        // SwitchState(new Player...State(this));
     }
 }
